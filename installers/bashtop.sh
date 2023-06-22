c49562988a55465d1a70a214c2ae38f9bba90804
Gabriel Gripp <550632+grippado@users.noreply.github.com>
2023-06-22 10:46:55 -0300

    :heavy_plus_sign: add bashtop

---
+++ installers/bashtop.sh
@@ -0,0 +1,6 @@
+echo '##################################################################'
+echo '####################  Installing BashTOP  ########################'
+echo '##################################################################'
+
+git clone https://github.com/aristocratos/bashtop.git ~/.bashtop
+sudo make ~/.bashtop/install
