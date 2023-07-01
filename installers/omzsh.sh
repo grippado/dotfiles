c186b6af1d502589dd7e895d0c6618c04109976a
Gabriel Gripp <550632+grippado@users.noreply.github.com>
2023-07-01 13:05:53 -0300

    add wakatime plugin to omzsh

--- installers/omzsh.sh
+++ installers/omzsh.sh
@@ -3,3 +3,11 @@ echo '###################  Installing Oh-My-ZSH  #######################'
 echo '##################################################################'
 
 cmd=$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)
+
+echo '##################################################################'
+echo '###################  Installing Oh-My-ZSH  #######################'
+echo '##################################################################'
+
+cd ~/.oh-my-zsh/custom/plugins && git clone https://github.com/sobolevn/wakatime-zsh-plugin.git wakatime
+
+cd ~/.dotfiles
